a759b3400be336cdb6dda5988285440ffd47fc14
Aphrodite0726 <54046103+Aphrodite0726@users.noreply.github.com>
2019-08-13 07:40:51 +0800

    Update 21101.js

--- univ/21101.js
+++ univ/21101.js
@@ -2,19 +2,25 @@
     school: "燕山大学",                      
     id: "21101",                              
     link: [ {
-                 name: "燕山大学主页",                      
+                 name: "燕山大学",                      
                  url: "https://www.ysu.edu.cn/" 
     },{
-                 name:"燕大教务系统",
-                 url:"http://202.206.243.185:8080/"
+                 name:"教务系统",
+                 url:"http://202.206.243.62/"
     },{
                  name: "上网认证",                     
                  url: "http://auth.ysu.edu.cn/"
     },{
-    	name:"燕大图书馆",
-    	url:"http://library.ysu.edu.cn/"
+    	         name:"图书馆",
+    	         url:"http://library.ysu.edu.cn/"
     },{
-    	name:"CET4&6",
-    	url:"http:cet.etest.net.cn/"
+    	         name:"CET报名",
+    	         url:"http://cet-bm.neea.edu.cn/"
+    },{
+    	         name:"CET成绩",
+    	         url:"http://cet.neea.edu.cn/cet/"
+    },{
+    	         name:"没品笑话",
+    	         url:"http://meipin.im/"
     }]
 };
